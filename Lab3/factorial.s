d41d31fd74f07f4ce125a6860091854d30274888
vanchav <43214862+vanchav@users.noreply.github.com>
2018-10-24 09:40:21 -0400

    Update factorial.s
    factorial code

--- Lab3/factorial.s
+++ Lab3/factorial.s
@@ -16,3 +16,22 @@ main:
 
 factorial:
 	# YOUR CODE HERE
+	 
+	 addi x2,x2,-16
+	 sw   x0,8(x2)
+	 sw   x10,0(x2)
+	 addi x6,x10,-1
+	 bge  x6,x0,Label1
+	 addi x10,x0,1
+	 addi x2,x2,16
+	 jalr x0,0(x1)
+     
+     	 Label1: 
+     	 addi x10,x10,-1
+	 jal  x1,factorial
+	 add  x5,x10,x0
+	 lw   x10,0(x2)
+	 lw   x1,8(x2)
+	 addi x2,x2,16
+	 mul  x10,x10,x5
+	 jalr x0,0(x1)
