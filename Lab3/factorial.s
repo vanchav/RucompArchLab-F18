8dec0eca83306554173b3a63ed769ef1f83f63f9
vanchav <43214862+vanchav@users.noreply.github.com>
2018-10-24 09:50:32 -0400

    with comments

--- Lab3/factorial.s
+++ Lab3/factorial.s
@@ -16,22 +16,23 @@ main:
 
 factorial:
 	# YOUR CODE HERE
-	 
-	 addi x2,x2,-16
-	 sw   x0,8(x2)
+	 addi x2,x2,-16 #move the pointer
+	 sw   x0,8(x2) 
 	 sw   x10,0(x2)
-	 addi x6,x10,-1
-	 bge  x6,x0,Label1
-	 addi x10,x0,1
-	 addi x2,x2,16
-	 jalr x0,0(x1)
+	 addi x6,x10,-1 #n-1
+	 bge  x6,x0,Label1 #if n is greater than 1
+	 addi x10,x0,1 
+	 addi x2,x2,16 #pop
+	 jalr x0,0(x1) #return val
      
-     	 Label1: 
-     	 addi x10,x10,-1
-	 jal  x1,factorial
-	 add  x5,x10,x0
-	 lw   x10,0(x2)
-	 lw   x1,8(x2)
+    	 Label1: 
+      	 addi x10,x10,-1 #n-1
+	 jal  x1,factorial #factorial of next number
+	 add  x5,x10,x0 #put factorial in x5
+	 lw   x10,0(x2) #restore pointer
+	 lw   x1,8(x2) 
 	 addi x2,x2,16
-	 mul  x10,x10,x5
-	 jalr x0,0(x1)
+	 mul  x10,x10,x5 #multiply n-1 factorial by n
+	 jalr x0,0(x1) #return 
+
+
